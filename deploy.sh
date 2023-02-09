a9c9ac0969c9c680751629413b35a2958bcff4ad
JR Duboc <jr.duboc@gmail.com>
2023-02-09 09:20:36 +0000

    Deploy fixes and chatbot

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# copy index.html to 404.html (otherwise router won't work for any page other than home page)
+cp index.html 404.html
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:JeanRemyDuboc/cmaconnect2.git master:gh-pages
+cd -
